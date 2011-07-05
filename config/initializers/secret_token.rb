2035a4718690e379cf404827b3f2c41ce23b2e7e
Douglas F Shearer <dougal.s@gmail.com>
2011-07-05 10:27:12 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sessiongrab::Application.config.secret_token = '6da9581820a4a6dcf77a5e05bd452ece56465cab0ba085454bc7ada1b70d0bdb74273c6af78809a07e4a90f417ff4f23a74c78fcd742f6abf64ab8d652b7cb0e'
